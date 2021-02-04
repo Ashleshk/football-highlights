22f059f71e38d233f3f5f43f983acee676d4b690
Ashleshk <ashleshuk@gmail.com>
2021-02-04 21:33:11 +0530

    Update navigation.js

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -6,3 +6,14 @@ const fetchTeams = () => $.ajax({
     url: 'http://api.football-data.org/v1/competitions/398/teams',
     headers: {'X-Auth-Token': API_KEY}
 })
+
+    .then(data => {
+        renderTeamList(data.teams.slice(0, 5))
+        const firstTeam = data.teams[0]
+        const href = firstTeam._links.self.href
+        fetchPlayerList(href.split('teams/')[1])
+        attachEvents()
+        return firstTeam
+    }, () => console.log('fail'))
+    .then(firstTeam => console.log(firstTeam))
+    .always(() => console.log('do some cleanup'))
