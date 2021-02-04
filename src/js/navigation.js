ab1883d2a1b6aa5d15517d982efc3206f5e58e49
Ashleshk <ashleshuk@gmail.com>
2021-02-04 21:32:53 +0530

    Update navigation.js

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -2,3 +2,7 @@ import $ from 'jquery'
 import { fetchPlayerList } from './teamInfo'
 import { API_KEY } from './config'
 
+const fetchTeams = () => $.ajax({
+    url: 'http://api.football-data.org/v1/competitions/398/teams',
+    headers: {'X-Auth-Token': API_KEY}
+})
